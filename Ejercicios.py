58799945e94b8f5163a8bfb2d762dcf32684c78c
Usuario <CORREO>
2025-06-16 17:20:18 +0200

    Final

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,6 +1,9 @@
 # 1. Crea una función de nombre enviarMensaje, que se le pasa por parámetro
 #    un nombre, y al ser llamada, imprime un mensaje con el nombre de la persona
 
+def enviarMensaje(nombre):
+    print(f"Hola, {nombre}")
+    
 '''
 OUTPUT:
 Hola, Fulano
@@ -10,6 +13,11 @@ Hola, Fulano
 
 # 2. Dada la siguiente lista de números, imprime línea a línea cada número
 numeros = [4, 3, 2, 5, 6]
+
+def imprimirNumeros(lista):
+    for numero in lista:
+        print(numero)
+imprimirNumeros(numeros)
 
 '''
 OUTPUT:
@@ -26,6 +34,12 @@ OUTPUT:
 #    el valor empieza en 5 y va reduciendo
 numero = 5
 
+def reducirNumero():
+    global numero
+    while numero >= 0:
+        print(f"El número ahora es {numero}")
+        numero -= 1
+
 '''
 OUTPUT:
 El número ahora es 5
@@ -41,12 +55,19 @@ El número ahora es 1
 #    y una vez indicados se llama a la función pasando los números que el usuario ha escrito.
 #    La función sumará los números y devolverá el resultado
 
+
+def sumar(numero1, numero2):
+    return numero1 + numero2
 print(f"SUMA: {sumar(numero1, numero2)}")
 
 
 
 # 5. Crea una función para que, cuando se le pase una lista cualquiera, imprima sus valores en orden inverso
 
+
+def imprimirListaInversa(lista):
+    for elemento in reversed(lista):
+        print(f"- {elemento}")
 nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
 imprimirListaInversa(nombres)
 '''
@@ -61,6 +82,9 @@ OUTPUT:
 # 6. Crear una función que busque una palabra en una lista, se le pasa la lista y la palabra a buscar
 #    Si la palabra existe, devuelve True, de lo contrario False
 
+
+def buscarPalabra(lista, palabra):
+    return palabra in lista
 print(buscarPalabra(["Fulano", "Mengano", "Zutano"], "Mengano")) # OUTPUT: True
 
 
@@ -68,6 +92,17 @@ print(buscarPalabra(["Fulano", "Mengano", "Zutano"], "Mengano")) # OUTPUT: True
 # 7. Crea un diccionario que contenga como clave tu nombre y como valor la cantidad de letras de la clave,
 #    lo mismo con el primer y segundo apellido, quedando un diccionario de tamaño 3. Luego, utiliza un bucle
 #    para imprimir el contenido
+
+
+def crearDiccionario(nombre, apellido1, apellido2):
+    diccionario = {
+        nombre: len(nombre),
+        apellido1: len(apellido1),
+        apellido2: len(apellido2)
+    }
+    return diccionario
+nombre = "Nombre"
+apellido1 = "Apellido1"
 
 '''
 OUTPUT:
